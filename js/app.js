f511c93eb40d8d5f787a649e6770419e177a35dc
tomvesa <47905734+tomvesa@users.noreply.github.com>
2022-07-15 02:05:20 +0200

    Refact + New
    game Object creation refactored
    
    New Ev Listener to Start Button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const game = new Game();
+game.phrases.push(new Phrase("This is Prase one"));
+game.phrases.push(new Phrase("This is Prase two"));
+game.phrases.push(new Phrase("This is Prase three"));
+game.phrases.push(new Phrase("This is Prase four"));
+game.phrases.push(new Phrase("This is Prase five"));
+
+
+
+const startBtn = document.getElementById("btn__reset");
+
+// Add event listener to Start button and bind "this" to a game object
+startBtn.addEventListener('click', game.startGame.bind(game));
+
+
+
