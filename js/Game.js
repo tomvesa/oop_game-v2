0de82970ec8a68077c2fa74f0494f59655bbb048
tomvesa <47905734+tomvesa@users.noreply.github.com>
2022-07-15 02:03:55 +0200

    New Features
    startGame Method
    getRandomPhrase Method

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+let overlay = document.getElementById('overlay');
+    
+
+
+class Game{
+    constructor(){
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
+
+    // hide overlay, select random phrase and display it on the dashboard
+    startGame(){
+        overlay.style.display = "none";
+        this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    handleInteractions(){}
+
+    removeLife(){}
+
+    checkForWin(){}
+
+    // choose activePhrase from this object phrases array
+    getRandomPhrase(){
+        const randomPhraseIndex =  Math.floor(Math.random() * this.phrases.length);
+        return this.activePhrase = this.phrases[randomPhraseIndex];
+    }
+
+     
+}    
